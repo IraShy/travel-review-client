17b22f072192feaa285bedf834ef55189fb4b312
triplehop <jwillcock00@gmail.com>
2020-07-09 14:26:43 +1000

    basic Nav link added

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Form } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/reviews">Reviews</Link>
-      <Link to="/reviews/create">Add a review</Link>
+      
+      <Navbar bg="dark" variant="dark">
+      <Nav className="justify-content-end" activekey="/">
+        {/* <Navbar fixed="top" /> */}
+        <Nav.Link href = "/">Home</Nav.Link>
+        <Nav.Link href = "/reviews">Reviews</Nav.Link>
+        <Nav.Link href = "/reviews/create">Add a review</Nav.Link>
+        </Nav>
+        </Navbar>
+      
+      
     </nav>
   );
 };
